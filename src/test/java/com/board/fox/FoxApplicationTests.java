b7cfe1d505d4710f07e059e0e880e5a065d25e7a
sangeun-jo <dog8536@naver.com>
2021-08-03 23:40:26 +0900

    환경 세팅

---
+++ src/test/java/com/board/fox/FoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.board.fox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
