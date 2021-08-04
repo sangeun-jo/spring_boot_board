99a5e760b658a1b16264dd497261b6d1d3280a12
sangeun-jo <dog8536@naver.com>
2021-08-04 18:07:34 +0900

    maven 프로젝트

---
+++ src/main/java/com/sej/firstboard/FirstboardApplication.java
@@ -0,0 +1,13 @@
+package com.sej.firstboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstboardApplication.class, args);
+	}
+
+}
