34d1029105203882c221c1f515cc0ec53bfcb106
sangeun-jo <dog8536@naver.com>
2021-08-13 22:45:37 +0900

    환경 설정

---
+++ src/main/java/com/example/firstboard/FirstboardApplication.java
@@ -0,0 +1,21 @@
+package com.example.firstboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
+
+@Controller
+@SpringBootApplication
+public class FirstboardApplication {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+      return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstboardApplication.class, args);
+    }
+}
