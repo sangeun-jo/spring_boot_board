add33d8d67bdf57ee4214bac18825ff317b85ebe
sangeun-jo <dog8536@naver.com>
2021-08-13 23:37:48 +0900

    db 연결

--- src/main/java/com/sej/firstboard/FirstboardApplication.java
+++ src/main/java/com/sej/firstboard/FirstboardApplication.java
@@ -1,5 +1,6 @@
-package com.example.firstboard;
+package com.sej.firstboard;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.*;
 
 @Controller
 @SpringBootApplication
+@MapperScan(value={"com.sej.firstboard.mapper"})
 public class FirstboardApplication {
 
     @RequestMapping("/")
---
+++ src/main/java/com/sej/firstboard/JspTest.java
@@ -0,0 +1,20 @@
+package com.sej.firstboard;
+
+import javax.annotation.Resource; 
+
+import org.springframework.stereotype.Controller; 
+import org.springframework.web.bind.annotation.RequestMapping; 
+
+import com.sej.firstboard.mapper.BoardMapper; 
+
+@Controller
+public class JspTest {
+    @Resource(name="com.sej.firstboard.mapper.BoardMapper")
+    BoardMapper mBoardMapper; 
+
+    @RequestMapping("/test")
+    private String jspTest() throws Exception{
+        System.out.println("boardCount: " + mBoardMapper.boardCount()); 
+        return "test"; 
+    }
+}
